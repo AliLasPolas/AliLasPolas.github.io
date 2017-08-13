16fc850a887275bf35a0a0499192160fd4cac5aa
Roy <nizamuddin.r.ali@gmail.com>
2017-08-13 17:13:09 +0200

    dynamique

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OPIRC</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Free Web tutorials">
+	<meta name="keywords" content="OPIRC, OP-IRC, Science, Culture, Érudition, Études, Textes, Association, Pierrefitte, Sur, Seine, Saint, Denis, 93">
+	<meta name="author" content="MD Nizamuddin Ali">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="asset/css/style.css">
+</head>
+<body>
+<div class="container global">
+	<header>
+		<div class="header_haut">
+			<div class="header_lettrage"><span class="opirc">OPIRC</span><span> - A la poursuite de la connaissance</span></div>
+			<div class="header_connexion">Connexion</div>
+		</div>
+		<div class="header_bas">
+			<div class="header_logo">
+				
+			</div>
+			<div class="header_menu">
+				<nav>
+					<ul class="nav_haut"><!-- 
+					--><li id="articles">Les articles</li><!--
+					--><li id="etudes">Les études</li><!--
+					--><li id="actus">Les actualités</li><!--
+					--><li id="propos">A propos</li><!--
+				--></ul>
+
+					<!-- CECI EST UN FIX A LA CON POUR RÉSOUDRE LES ESPACES ENTRE LES LI -->
+					<ul class="nav_bas"><!--
+					--><li id="rejoindre">Nous rejoindre</li><!--
+					--><li id="planning">Planning</li><!--
+					--><li id="contact">Contact</li><!--
+					--><li id="recherche">Recherche</li><!--
+					--><li id="faq">FAQ</li><!--
+					--><li id="contenu">Créer du contenu </li><!--
+				--></ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<main>
+			<h1>L'accélérateur de particules de l'OPIRC</h1>
+		<section class="main">
+			<figure>
+				<img src="asset/img/lhc.jpg">
+				<figcaption>On peut toujours réver ...</figcaption>
+			</figure>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+			<br>
+			<br>
+			<br>
+
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+			<br>
+			<br>
+			<br>
+
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+			<br>
+			<br>
+			<br>
+
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+			<br><br><br>
+
+			Roy MD Nizamuddin
+			</p>
+		</section>
+		<section class="droite">
+			<div class="menu_droite">
+				<ul>
+					<li class="main_li">
+						<ul>
+							<li>L'accélérateur</li>
+							<li>LHC - Grand collisonneur de hadrons</li>
+							<li>SPS - Supersynchrotron à protons</li>
+							<li>LMDF - La maman de Franklin </li>
+						</ul>
+					</li>
+					<li class="main_li">
+						<ul>
+							<li>Sathish "Ben" Mourty</li>
+							<li>Je l'ai vu aujourd'hui</li>
+							<li>Il avait pas les cheveux longs</li>
+							<li>M'aurait-on menti ?</li>
+						</ul>
+					</li>
+					<li class="main_li">
+						<ul>
+							<li>J'aime les frites</li>
+							<li>Et le poulet</li>
+							<li>Ainsi que les nems</li>
+							<li>Mais pas les sushis</li>
+						</ul>
+					</li>
+					<li class="main_li">
+						<ul>
+							<li>Quoi d'autre ?</li>
+							<li>Je n'ai plus trop d'idée</li>
+							<li>C'est la fin de ce menu</li>
+							<li>Maintenant</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</section>
+	</main>
+	<div class="clear"></div>
+	<footer>
+		<p>© Copyright OPIRC 2017 | Déclaration CNIL | Mentions Légales <br>
+		Organisation pour initier la recherche et la connaissance (OPIRC) - Pierrefitte Sur Seine - 93380</p>
+	</footer>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="asset/js/script.js"></script>
+</body>
+</html>
